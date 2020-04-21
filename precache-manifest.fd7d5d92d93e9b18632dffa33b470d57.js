216cee38aefbd64553da7f627cf47400c5f3febe
TygerrTygerr <tygerdiago@aol.com>
2020-04-21 17:20:31 -0400

    Updates

---
+++ precache-manifest.fd7d5d92d93e9b18632dffa33b470d57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f6bd644a9d97035fcac1c296cafcef2",
+    "url": "/tygerrtygerr.github.io/index.html"
+  },
+  {
+    "revision": "dc19e47b69e7e2d6cc03",
+    "url": "/tygerrtygerr.github.io/static/css/main.f4976fff.chunk.css"
+  },
+  {
+    "revision": "b8f80746b2806ccb3b85",
+    "url": "/tygerrtygerr.github.io/static/js/2.f9116045.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/tygerrtygerr.github.io/static/js/2.f9116045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc19e47b69e7e2d6cc03",
+    "url": "/tygerrtygerr.github.io/static/js/main.eab4876e.chunk.js"
+  },
+  {
+    "revision": "746a899503f395c4903c",
+    "url": "/tygerrtygerr.github.io/static/js/runtime-main.119aabbf.js"
+  },
+  {
+    "revision": "8c037dd6ac81ee8d6104d2aa307e3a46",
+    "url": "/tygerrtygerr.github.io/static/media/lillian.8c037dd6.jpg"
+  },
+  {
+    "revision": "789314a3717b4837a115a49d0743d378",
+    "url": "/tygerrtygerr.github.io/static/media/me.789314a3.jpg"
+  }
+]);
